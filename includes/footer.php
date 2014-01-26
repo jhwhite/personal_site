a3deee31123c58d47ec97b516cf7f8d1597b82ac
J White <jody@jody-white.com>
2014-01-26 18:49:24 -0500

    update version of jquery to fix bug in chrome where buttons were listed up and down instead of a row

--- includes/footer.php
+++ includes/footer.php
@@ -14,7 +14,7 @@
     <!-- Placed at the end of the document so the pages load faster -->
     
     
-    <script src="http://code.jquery.com/jquery-1.8.3.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script src="js/bootstrap.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
     <script src='js/ability.js'></script>
