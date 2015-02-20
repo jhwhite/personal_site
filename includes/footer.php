48d18d0ee61d777cb8d2c04e02338ce94684f436
James White <jody@jody-white.com>
2015-02-20 12:15:29 -0500

    Added mixpnael badge to footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,8 @@
       <footer>
         <p>&copy; <?php echo date('Y'); ?></br>
         <?php if ($section == "calc") {echo "This website uses trademarks and/or copyrights owned by Paizo Publishing, LLC, which are used under Paizo's Community Use Policy. We are expressly prohibited from charging you to use or access this content. This website is not published, endorsed, or specifically approved by Paizo Publishing. For more information about Paizo's Community Use Policy, please visit paizo.com/communityuse. For more information about Paizo Publishing and Paizo products, please visit paizo.com.</br>";} ?>
-        Social Media Icons were created by Bruno Maia, IconTexto http://www.icontexto.com
+        Social Media Icons were created by Bruno Maia, IconTexto http://www.icontexto.com<br />
+<a href='https://mixpanel.com/f/partner'><img src='//cdn.mxpnl.com/site_media/images/partner/badge_light.png' alt='Mobile Analytics' /></a><br />
         </p>
 
       </footer>
